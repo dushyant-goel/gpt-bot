78c7a6e52cecbb3c3b49f53780f5b50ceb813180
Dushyant Goel <dushyant95goel@gmail.com>
2023-06-18 14:26:25 +0530

    fun prompt

--- routes/api.js
+++ routes/api.js
@@ -6,38 +6,35 @@ const axios = require('axios')
 router.get('/generate', async (req, res) => {
 
   try {
-    // const response = await axios.post(`https://api.openai.com/v1/chat/completions`,
-    //   {
-    //     "model": "gpt-3.5-turbo",
-    //     "messages": [
-    //       {
-    //         "role": "system",
-    //         "content": "You are Amitabh Bachchan"
-    //       },
-    //       {
-    //         "role": "user",
-    //         "content": "I loved your film Shahenshah, can you repeat your most famous dialogue from it."
-    //       }
-    //     ],
-    //     "temperature": 1,
-    //     "top_p": 1,
-    //     "n": 1,
-    //     "stream": false,
-    //     "max_tokens": 250,
-    //     "presence_penalty": 0,
-    //     "frequency_penalty": 0
-    //   }, {
-    //   headers: {
-    //     'Authorization': 'Bearer <token>',
-    //     'Content-Type': 'application/json'
-    //   }
-    // });
+    const response = await axios.post(`https://api.openai.com/v1/chat/completions`,
+      {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+          {
+            "role": "system",
+            "content": "You are Amitabh Bachchan"
+          },
+          {
+            "role": "user",
+            "content": "I loved your film Shahenshah, can you repeat your most famous dialogue from it."
+          }
+        ],
+        "temperature": 1,
+        "top_p": 1,
+        "n": 1,
+        "stream": false,
+        "max_tokens": 250,
+        "presence_penalty": 0,
+        "frequency_penalty": 0
+      }, {
+      headers: {
+        'Authorization': 'Bearer <token>',
+        'Content-Type': 'application/json'
+      }
+    });
 
     // Handle the API response
-    // generatedText = response.data.choices[0].message.content;
-    
-    const apiKey = process.env.API_KEY;
-    console.log(apiKey);
+    generatedText = response.data.choices[0].message.content;
     res.send(apiKey);
 
   } catch (error) {
